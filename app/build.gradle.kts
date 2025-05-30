4312059890ff96b20127391765e8dc26a90729e4
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2025-05-31 01:36:42 +0600

    bump version to 3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.shahriyardx.doto"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
