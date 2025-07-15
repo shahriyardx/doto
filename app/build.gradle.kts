95e10181f230fcce21805054b74d20986d3fef13
shahriyardx <mdshahriyaralam552@gmail.com>
2025-07-15 17:53:31 +0600

    updated deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
-    id("com.google.devtools.ksp") version "2.1.21-2.0.1" apply true
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply true
 }
 
 android {
